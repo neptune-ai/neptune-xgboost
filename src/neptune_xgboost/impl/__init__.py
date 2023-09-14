36c7955dae9d9b7687da9851685646901299b784
Sabine <sabine.nyholm@neptune.ai>
2023-09-14 09:34:08 +0300

    make graphviz error more helpful

--- src/neptune_xgboost/impl/__init__.py
+++ src/neptune_xgboost/impl/__init__.py
@@ -128,7 +128,9 @@ class NeptuneCallback(xgb.callback.TrainingCallback):
                 self.log_tree = None
                 message = (
                     "Graphviz executables not found, so trees will not be logged. "
-                    "Make sure the Graphviz executables are on your systems' PATH"
+                    "Make sure the Graphviz executables are on your systems' PATH. "
+                    "Note that you need to have both the Graphviz software and the Python interface installed. "
+                    "For installation details, see the integration docs: https://docs.neptune.ai/integrations/xgboost/"
                 )
                 warnings.warn(message)
 
